03f354d5607515b58b54e325d9f625768c4f6c80
Fernando de la Fuente <delafuentecarrasco@gmail.com>
2022-05-01 19:13:37 -0400

    falso

--- gestorbackend/settings.py
+++ gestorbackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a&@a70#5_jtzeso#hn5r^!w&###i#(zna@brmwl)9t$9--cvc0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['djangoapp-env.eba-wz2bmap9.us-west-2.elasticbeanstalk.com','127.0.0.1']
 
