204bd43a19ad8df89e75acd2841fd515e3edbd56
Fernando de la Fuente <delafuentecarrasco@gmail.com>
2022-05-22 17:16:56 -0400

    req

--- gestorbackend/settings.py
+++ gestorbackend/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'gestorbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gestor',
+        'USER': 'postgres',
+        'PASSWORD': 'nimda12345',
+        'HOST': 'gestordb.cq27bj5hbx7y.us-west-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
